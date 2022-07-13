d101b948c4541ec3b3c6658ef829998ab8b24cf5
dhvihan <danulvihan@gmail.com>
2022-07-14 00:56:36 +0530

    paynow done

--- lib/pages/paynow.dart
+++ lib/pages/paynow.dart
@@ -1,7 +1,8 @@
-// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers
+// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 import 'package:group2/common/size.dart';
+import 'package:group2/pages/ppayment.dart';
 
 
 
@@ -351,28 +352,294 @@ class _paynowState extends State<paynow> {
             ),
               Container(
               //product summary
-              child: Row(
-                children: [
-                    Container(
-                      //image
-                      alignment: Alignment.centerLeft,
-                      width: 110,
-                      height: 100,
-                      decoration: BoxDecoration(
-                        border:
-                            Border.all(width: 1, color: Colors.grey.shade200),
-                        borderRadius: BorderRadius.circular(20),
-                        color: Colors.white,
-                        image: DecorationImage(
-                          image: AssetImage("assets/imgs/logo.png"),
-                          fit: BoxFit.cover,
+                  child: Row(
+                    children: [
+                        Container(
+                          //image
+                          margin: EdgeInsets.fromLTRB(15,0,15,10),
+                          alignment: Alignment.center,
+                          width: 150,
+                          height: 120,
+                          decoration: BoxDecoration(
+                            border:
+                                Border.all(width: 1, color: Colors.grey.shade200),
+                            borderRadius: BorderRadius.circular(20),
+                            color: Colors.white,
+                            image: DecorationImage(
+                              image: AssetImage("assets/imgs/logo.png"),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
                         ),
-                      ),
-                    ),
-                          
-                ],
+                        Container(
+                          //item details
+                          child: Column(
+                            children: [
+                              Container(
+                                //item name
+                                margin: EdgeInsets.fromLTRB(15,0,15,20),
+                                alignment: Alignment.topCenter,
+                                child: Text("Item Name",style: TextStyle(
+                                fontFamily: "Poppins",
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                  height: 1.15
+                              ),), 
+                              ),
+                              Container(
+                                //price and qty
+                                child: Row(
+                                  children: [
+                                    Container(
+                                      //price
+                                      margin: EdgeInsets.fromLTRB(15,0,15,10),
+                                      alignment: Alignment.topCenter,
+                                      child: Text("Rs.10,500",style: TextStyle(
+                                      fontFamily: "Poppins",
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.normal,
+                                        height: 1.15
+                                    ),), 
+                                    ),
+                                    Container(
+                                      //price
+                                      margin: EdgeInsets.fromLTRB(15,0,15,10),
+                                      alignment: Alignment.topCenter,
+                                      child: Text("Qty:",style: TextStyle(
+                                      fontFamily: "Poppins",
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.normal,
+                                        height: 1.15
+                                    ),), 
+                                    ),
+
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        )
+                              
+                    ],
+                  ),
+               ),
+              Container(
+                //hr line
+                margin: EdgeInsets.all(15),
+                
+                decoration: BoxDecoration(
+                  
+                                border: Border(
+                                  bottom: BorderSide(color: Colors.grey),
+                                  
+                                ),
+                              ),
               ),
-            ),
+              Container(
+                child: Column(
+                  children: [
+                     Container(
+                      //total
+                       child: Row(
+                        
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                         children: [
+                              Container(
+                                          //item total word
+                                          margin: EdgeInsets.fromLTRB(30,10,15,20),
+                                          alignment: Alignment.topLeft,
+                                          child: Text("Item Total",style: TextStyle(
+                                          fontFamily: "Poppins",
+                                            fontSize: 15,
+                                            fontWeight: FontWeight.normal,
+                                            height: 1.15
+                                        ),), 
+                                        ),
+                              Container(
+                                          //item total price
+                                          margin: EdgeInsets.fromLTRB(30,10,30,20),
+                                          alignment: Alignment.topRight,
+                                          child: Text("Rs.10,500",style: TextStyle(
+                                          fontFamily: "Poppins",
+                                            fontSize: 15,
+                                            fontWeight: FontWeight.normal,
+                                            height: 1.15
+                                        ),), 
+                                        ),
+                         ],
+                       ),
+                     ),
+                      
+                     Container(
+                      //delivery
+                       child: Row(
+                        
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                         children: [
+                              Container(
+                                          //delivery cost word
+                                          margin: EdgeInsets.fromLTRB(30,0,15,20),
+                                          alignment: Alignment.topLeft,
+                                          child: Text("Delivery Cost",style: TextStyle(
+                                          fontFamily: "Poppins",
+                                            fontSize: 15,
+                                            fontWeight: FontWeight.normal,
+                                            height: 1.15
+                                        ),), 
+                                        ),
+                              Container(
+                                          //delivery cost
+                                          margin: EdgeInsets.fromLTRB(30,0,30,20),
+                                          alignment: Alignment.topRight,
+                                          child: Text("Rs.119",style: TextStyle(
+                                          fontFamily: "Poppins",
+                                            fontSize: 15,
+                                            fontWeight: FontWeight.normal,
+                                            height: 1.15
+                                        ),), 
+                                        ),
+                         ],
+                       ),
+                     ),
+                     Container(
+                        //hr line
+                        margin: EdgeInsets.all(15),
+                        
+                        decoration: BoxDecoration(
+                          
+                                        border: Border(
+                                          bottom: BorderSide(color: Colors.grey),
+                                          
+                                        ),
+                                      ),
+                      ),
+                     Container(
+                      margin: EdgeInsets.fromLTRB(30,10,30,20),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                            Container(
+                              //total final display bottom
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Container(
+                                    //total word
+                                    margin: EdgeInsets.all(10),
+                                    child: Text("Total",style: TextStyle(
+                                            fontFamily: "Poppins",
+                                              fontSize: 25,
+                                              fontWeight: FontWeight.bold,
+                                              height: 1.15
+                                          ),),
+                                  ), 
+                                  Container(
+                                    //final price
+                                    margin: EdgeInsets.all(5),
+                                    child: Text("Rs.10,619",style: TextStyle(
+                                            fontFamily: "Poppins",
+                                              fontSize: 25,
+                                              fontWeight: FontWeight.bold,
+                                              height: 1.15,
+                                              color: Colors.blue,
+                                          ),),
+                                  ), 
+                                ],
+                              ),
+                            ),
+                            Container(
+                                  //seller details
+                                  
+                                   margin: const EdgeInsets.fromLTRB(0, 0, 0, 20),
+                                    width: kPropWidth(context, 0.32),
+                                    height: kPropHeight(context, 0.08),
+                                  decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    //  border: Border.all(width: 2, color: Colors.grey.shade200),
+                                    borderRadius: const BorderRadius.all(
+                                        Radius.circular(20)),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Colors.grey.withOpacity(0.25),
+                                        spreadRadius: 10,
+                                        blurRadius: 7,
+                                        offset: const Offset(
+                                            2, 5), // changes position of shadow
+                                      ),
+                                      BoxShadow(
+                                        color: Colors.white.withOpacity(0.8),
+                                        spreadRadius: 10,
+                                        blurRadius: 7,
+
+                                        offset: const Offset(-10,
+                                            -10), // changes position of shadow
+                                      ),
+                                    ],
+                                  ),
+                                  child: ElevatedButton(
+                                    onPressed: () {
+                                      Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) =>
+                                                ppayment()),
+                                      );
+                                    },
+                                    style: ElevatedButton.styleFrom(
+                                      primary: Colors.greenAccent[200],
+                                      shadowColor:
+                                          Colors.grey.withOpacity(0.25),
+                                      padding: const EdgeInsets.symmetric(
+                                          vertical: 0.0, horizontal: 0.0),
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(20.0),
+                                      ),
+                                    ),
+                                    child: Row(
+                                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                      children: [
+                                        
+                                        Container(
+                                          //details of a item
+                                          margin: EdgeInsets.fromLTRB(0,25,0,0),
+                                           width: kPropWidth(context, 0.2),
+                                           height: kPropHeight(context, 0.1),
+                                           alignment: Alignment.center,
+                                          child: Column(
+                                            
+                                            children: [
+                                              Container(
+                                                child: const Text(
+                                                  "Proceed",
+                                                  textAlign: TextAlign.right,
+                                                  style: TextStyle(
+                                                      fontFamily: "poppins",
+                                                      fontSize: 16,
+                                                      color: Colors.black87,
+                                                      fontWeight:
+                                                          FontWeight.bold,
+                                                      height: 1.15),
+                                                ),
+                                              ),
+                                              
+                                            ],
+                                          ),
+                                        ),
+                                        Container(
+                                          child: Icon(Icons.arrow_forward_ios_rounded, color: Colors.green,),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              
+                        ],
+                      ),
+                     )
+                  ],
+                ),
+              ),
           ],
         ),
       ),
---
+++ lib/pages/ppayment.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ppayment extends StatefulWidget {
+  const ppayment({Key? key}) : super(key: key);
+
+  @override
+  State<ppayment> createState() => _ppaymentState();
+}
+
+class _ppaymentState extends State<ppayment> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
