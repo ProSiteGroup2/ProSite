dbf7c995bdeef5c558a9eb59da66f4cd52863651
Muthahhar97 <muthahharrdx@hotmail.com>
2022-07-20 13:20:49 +0530

    SP login front, back connect

--- lib/Classes/authenticate_service.dart
+++ lib/Classes/authenticate_service.dart
@@ -3,15 +3,30 @@ import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
 
-
 class AuthService{
   Dio dio=new Dio();
 
   consumerLogin(email, password) async {
     try{
-      return await dio.post('https://prositegroup2.herokuapp.com/consumerLogin',
+      return await dio.post('https://prositegroup2.herokuapp.com/SPLogin',
       data: {"email":email,"password":password},
       options: Options(contentType: Headers.jsonContentType));
+    } on DioError catch(e){
+      Fluttertoast.showToast(
+          msg: e.response?.data['msg'],
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM,
+          backgroundColor: Colors.red,
+          textColor: Colors.white,
+          fontSize: 16.0);
+    }
+  }
+
+  SPLogin(email, password) async {
+    try{
+      return await dio.post('https://prositegroup2.herokuapp.com/SPLogin',
+          data: {"email":email,"password":password},
+          options: Options(contentType: Headers.jsonContentType));
     } on DioError catch(e){
       Fluttertoast.showToast(
           msg: e.response?.data['msg'],
@@ -46,6 +61,7 @@ class AuthService{
           fontSize: 16.0);
     }
   }
+
 
 
 }
