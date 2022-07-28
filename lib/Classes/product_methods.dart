5b8d3bd472b933a51c55a95ffee7ac5f736cf55d
Muthahhar97 <muthahharrdx@hotmail.com>
2022-07-28 19:18:11 +0530

    create a product class to add product dio methods

---
+++ lib/Classes/product_methods.dart
@@ -0,0 +1,67 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class ProductMethods{
+  Dio dio=new Dio();
+
+  addProduct(productname,price,stock,size,category,description,seller) async {
+    try {
+      return await dio.post('https://prositegroup2.herokuapp.com/addProduct',
+      data:{
+        "productname":productname,
+        "price":price,
+        "stock":stock,
+        "size":size,
+        "category":category,
+        "description":description,
+        "seller":seller
+      },options: Options(contentType: Headers.jsonContentType));
+    } on DioError catch (e) {
+      Fluttertoast.showToast(
+          msg: e.response?.data['msg'],
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM,
+          backgroundColor: Colors.red,
+          textColor: Colors.white,
+          fontSize: 16.0);
+    }
+  }
+
+  productImage(File file,productID) async {
+    try {
+      String fileName=file.path.split('/').last;
+      FormData formData=FormData.fromMap({
+        "image": await MultipartFile.fromFile(file.path, filename:fileName),
+      });
+
+      return await dio.put('https://prositegroup2.herokuapp.com/productImage/$productID', data: formData);
+    } on DioError catch (e) {
+      Fluttertoast.showToast(
+          msg: 'image upload failed',
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM,
+          backgroundColor: Colors.red,
+          textColor: Colors.white,
+          fontSize: 16.0);
+    }
+  }
+
+  getProductInfo(productID) async {
+    try {
+      return await dio.get('https://prositegroup2.herokuapp.com/getProductInfo/$productID');
+    } on DioError catch (e) {
+      Fluttertoast.showToast(
+          msg: e.response?.data['msg'],
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM,
+          backgroundColor: Colors.red,
+          textColor: Colors.white,
+          fontSize: 16.0);
+    }
+  }
+
+
+}
