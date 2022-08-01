18f9d3128adb8ce5918049ead66dff1604a246cd
dhvihan <danulvihan@gmail.com>
2022-08-01 18:07:06 +0530

    Revert "frame"
    This reverts commit e7399fb2e42d5d0261706f91d4099f80a2dd5169.

--- android/app/src/main/kotlin/com/example/group2/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/group2/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.group2
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragementActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragementActivity() {
 }
