2cf5b83d5afef1b8a8107fdf76845f8dc6fde91a
dhvihan <danulvihan@gmail.com>
2022-08-02 10:49:01 +0530

    Revert "sdk version & fragment activity"
    This reverts commit 234358fb3ad87d93a019c8cfc68fed1de104dd4f.

--- android/app/src/main/kotlin/com/example/group2/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/group2/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.group2
 
-import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterFragmentActivity() {
+class MainActivity: FlutterActivity() {
 }
