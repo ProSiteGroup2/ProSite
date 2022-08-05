e251bebdf00fc7a35bc1275d1b52b7373f745b68
Muthahhar97 <muthahharrdx@hotmail.com>
2022-08-05 19:33:42 +0530

    add Appointment dio method added

--- lib/Classes/service_provider_methods.dart
+++ lib/Classes/service_provider_methods.dart
@@ -6,9 +6,10 @@ import 'package:fluttertoast/fluttertoast.dart';
 
 
 class SPMethods{
+  
   Dio dio=new Dio();
 
-  spFeedback(feedback) async {
+  addFeedback(feedback) async {
     try{
 
       return await dio.post('https://prositegroup2.herokuapp.com/getFeedback',
@@ -25,6 +26,27 @@ class SPMethods{
       fontSize: 16.0);
      
       
+    }
+  }
+
+  addAppointment(consumerID,sp_email,app_date,app_time) async {
+    try {
+      return await dio.post('https://prositegroup2.herokuapp.com/addAppointment',
+      data: {
+        "consumer":consumerID,
+        "sp_email":sp_email,
+        "date":app_date,
+        "time":app_time
+      },
+      options: Options(contentType: Headers.jsonContentType));
+    } on DioError catch (e) {
+      Fluttertoast.showToast(
+          msg: e.response?.data['msg'],
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM,
+          backgroundColor: Colors.red,
+          textColor: Colors.white,
+          fontSize: 16.0);
     }
   }
 
--- lib/pages/customer_hdprofileview.dart
+++ lib/pages/customer_hdprofileview.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
-import 'package:group2/pages/feedback%20view.dart';
 import 'package:group2/pages/feedback_hardware.dart';
 import 'package:group2/pages/pick_date.dart';
 import 'package:url_launcher/url_launcher.dart';
