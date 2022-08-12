366097758c788436fd5cad4a26b6519c64e1b6c6
Muthahhar97 <muthahharrdx@hotmail.com>
2022-08-12 10:43:03 +0530

    consumer,sp activities

--- lib/pages/labour_activities.dart
+++ lib/pages/labour_activities.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:group2/Classes/labour_schedule.dart';
 // import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:group2/Classes/my_flutter_app_icons.dart';
+import 'package:group2/Classes/service_provider_methods.dart';
 import 'package:url_launcher/url_launcher.dart';
+
+import '../globals.dart';
 
 
 
@@ -14,18 +18,73 @@ class LActivity extends StatefulWidget {
 }
 
 class _LActivityState extends State<LActivity> {
+
+
+  @override
+  List l_schedule=[];
+  List l_preActivity=[];
+
+  @override
+  void initState() {
+    gettingSPPastAppointments();
+    gettingSPUpcomingAppointments();
+    super.initState();
+  }
+
+  String alert_pre='';
+  String alert_up='';
+
+  Future<void> gettingSPPastAppointments() async {
+    var results=await SPMethods().getSPPastAppointment(sp['email']);
+    if(results.data['success']){
+      setState(() {
+        l_preActivity=results.data['appointments'];
+      });
+      if(l_preActivity.isEmpty){
+        setState(() {
+          alert_pre='There are no Appointments';
+        });
+      }else{
+        print(l_preActivity);
+      }
+    }else{
+      Fluttertoast.showToast(
+          msg: 'getting Past Appointments failed',
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM,
+          backgroundColor: Colors.red,
+          textColor: Colors.white,
+          fontSize: 16.0);
+    }
+  }
+
+  Future<void> gettingSPUpcomingAppointments() async {
+    var results=await SPMethods().getSPUpcomingAppointment(sp['email']);
+    if(results.data['success']){
+      setState(() {
+        l_schedule=results.data['appointments'];
+      });
+      if(l_schedule.isEmpty){
+        setState(() {
+          alert_up='There are no Appointments';
+        });
+      }else{
+        print(l_schedule);
+      }
+    }else{
+      Fluttertoast.showToast(
+          msg: 'getting Upcoming Appointments failed',
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM,
+          backgroundColor: Colors.red,
+          textColor: Colors.white,
+          fontSize: 16.0);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    List<LSchedule> l_schedule=[
-      LSchedule('Saman Kumara', '2022-05-06', '1.00 pm','worker1.jpg'),
-      LSchedule('Kiri Saman', '2022-05-11', '10.00 am','worker2.jpg'),
-      LSchedule('Lahiru Kumara', '2022-05-06', '4.00 pm','worker3.jpg'),
-    ];
-    List<LSchedule> l_preActivity=[
-      LSchedule('Saman Kumara', '2022-05-06', '1.00 pm','worker1.jpg'),
-      LSchedule('Kiri Saman', '2022-05-11', '10.00 am','worker2.jpg'),
-      LSchedule('Lahiru Kumara', '2022-05-06', '4.00 pm','worker3.jpg'),
-    ];
+
     List<int> ratings=[4,4,4,3,5,3];
     int sum_rating=0;
     for (var i = 0;i<ratings.length;i++){
@@ -33,7 +92,6 @@ class _LActivityState extends State<LActivity> {
     }
     var avg_rating= (sum_rating/ratings.length);
     avg_rating=double.parse(avg_rating.toStringAsFixed(2));
-
     return Scaffold(
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
@@ -126,7 +184,7 @@ class _LActivityState extends State<LActivity> {
                   ),
                   height: 250,
                   width: 400,
-                  child: ListView.builder(
+                  child: (alert_up!='')? Center(child: Text(alert_up,style: TextStyle(color: Colors.black),)) : ListView.builder(
                     scrollDirection: Axis.vertical,
                     itemCount: l_schedule.length,
                     itemBuilder: (context,index){
@@ -150,8 +208,7 @@ class _LActivityState extends State<LActivity> {
                                 borderRadius: BorderRadius.circular(10),
                                 color: Colors.white,
                                 image: DecorationImage(
-                                  image: AssetImage(
-                                      "assets/imgs/${l_schedule[index].profile}"),
+                                  image: NetworkImage('${l_schedule[index]['consumer']['imageUrl']}'),
                                   fit: BoxFit.cover,
                                 ),
                               ),
@@ -165,7 +222,7 @@ class _LActivityState extends State<LActivity> {
                                     crossAxisAlignment: CrossAxisAlignment.start,
                                     children: [
                                       Text(
-                                          l_schedule[index].cName,
+                                        l_schedule[index]['consumer']['username'],
                                         style: TextStyle(
                                           fontFamily: 'Poppins',
                                           fontSize: 13.0,
@@ -177,7 +234,7 @@ class _LActivityState extends State<LActivity> {
                                         children: [
                                           Expanded(
                                             child: Text(
-                                              l_schedule[index].date,
+                                              l_schedule[index]['date'].toString().substring(0,10),
                                               style: TextStyle(
                                                 fontFamily: 'Poppins',
                                                 color: Colors.black54,
@@ -188,7 +245,7 @@ class _LActivityState extends State<LActivity> {
                                           SizedBox(width: 5,),
                                           Expanded(
                                               child:Text(
-                                                l_schedule[index].time,
+                                                '${l_schedule[index]['time']} (24 Hours)',
                                                 style: TextStyle(
                                                   fontFamily: 'Poppins',
                                                   fontSize: 12.0,
@@ -209,7 +266,9 @@ class _LActivityState extends State<LActivity> {
                                     icon: Icon(
                                         Icons.add_call
                                     ),
-                                    onPressed: (){},
+                                    onPressed: (){
+                                      launch("tel://+94${l_schedule[index]['consumer']['contactNo'].toString().substring(1)}");
+                                    },
                                     color: Colors.black,
                                   ),),
                                 Expanded(
@@ -219,7 +278,9 @@ class _LActivityState extends State<LActivity> {
                                     icon: Icon(
                                       MyFlutterApp.whatsapp
                                     ),
-                                    onPressed: (){},
+                                    onPressed: (){
+                                      launchwhatsapp( number:"tel://+94${l_schedule[index]['consumer']['contactNo'].toString().substring(1)}",message: "Hello");
+                                    },
 
                                     color: Colors.green,
                                   ),)
@@ -253,7 +314,7 @@ class _LActivityState extends State<LActivity> {
                   ),
                   height: 250,
                   width: 400,
-                  child: ListView.builder(
+                  child: (alert_pre!='')? Center(child: Text(alert_pre,style: TextStyle(color: Colors.black),)) : ListView.builder(
                     scrollDirection: Axis.vertical,
                     itemCount: l_preActivity.length,
                     itemBuilder: (context,index){
@@ -277,8 +338,7 @@ class _LActivityState extends State<LActivity> {
                                 borderRadius: BorderRadius.circular(10),
                                 color: Colors.white,
                                 image: DecorationImage(
-                                  image: AssetImage(
-                                      "assets/imgs/${l_preActivity[index].profile}"),
+                                  image: NetworkImage('${l_preActivity[index]['consumer']['imageUrl']}'),
                                   fit: BoxFit.cover,
                                 ),
                               ),
@@ -292,7 +352,7 @@ class _LActivityState extends State<LActivity> {
                                     crossAxisAlignment: CrossAxisAlignment.start,
                                     children: [
                                       Text(
-                                        l_preActivity[index].cName,
+                                        l_preActivity[index]['consumer']['username'],
                                         style: TextStyle(
                                             fontFamily: 'Poppins',
                                             fontSize: 13.0,
@@ -304,7 +364,7 @@ class _LActivityState extends State<LActivity> {
                                         children: [
                                           Expanded(
                                             child: Text(
-                                              l_preActivity[index].date,
+                                              l_preActivity[index]['date'].toString().substring(0,10),
                                               style: TextStyle(
                                                 fontFamily: 'Poppins',
                                                 color: Colors.black54,
@@ -315,7 +375,7 @@ class _LActivityState extends State<LActivity> {
                                           SizedBox(width: 5,),
                                           Expanded(
                                               child:Text(
-                                                l_preActivity[index].time,
+                                                '${l_preActivity[index]['time']} (24 Hours)',
                                                 style: TextStyle(
                                                   fontFamily: 'Poppins',
                                                   fontSize: 12.0,
@@ -337,7 +397,7 @@ class _LActivityState extends State<LActivity> {
                                       Icons.add_call
                                     ),
                                     onPressed: (){
-                                      launch("tel://+94787145867");
+                                      launch("tel://+94${l_preActivity[index]['consumer']['contactNo'].toString().substring(1)}");
                                     },
                                     color: Colors.black,
                                   ),),
@@ -350,7 +410,7 @@ class _LActivityState extends State<LActivity> {
                                         MyFlutterApp.whatsapp,
                                     ),
                                     onPressed: (){
-                                      launchwhatsapp( number:"tel://+94787145867",message: "hello");
+                                      launchwhatsapp( number:"tel://+94${l_preActivity[index]['consumer']['contactNo'].toString().substring(1)}",message: "Hello");
                                     },
                                     color: Colors.green,
                                   ),)
