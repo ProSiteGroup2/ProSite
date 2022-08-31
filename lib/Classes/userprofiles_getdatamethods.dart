7eb2e6df8fe229dba5d5c3bb635df3e0514b8c32
krishan-indrajith <gwkrishanindrajithw@gmail.com>
2022-08-31 15:19:01 +0530

    add method

--- lib/Classes/userprofiles_getdatamethods.dart
+++ lib/Classes/userprofiles_getdatamethods.dart
@@ -24,4 +24,24 @@ class GetUsersData {
       }
   }
 
+  Future<Map<String, dynamic>?> getHardwareInfo(String url) async {
+    try {
+      var client = http.Client();
+      final response = await client.get(Uri.parse(url), headers:{
+        'Content-Type': 'application/json',
+        HttpHeaders.authorizationHeader:'Bearer $token',
+      });
+      if(jsonDecode(response.body)['success']){
+        return jsonDecode(response.body)['sp'];
+      }else{
+        throw jsonDecode(response.body)['error'];
+      }
+    }
+    catch(err){
+      print(err.toString());
+      throw err.toString();
+    }
+  }
+
+
 }
